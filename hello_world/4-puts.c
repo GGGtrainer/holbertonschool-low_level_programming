bebdee1dc933811abc82c34b52ba48b82fbaadba
GGGtrainer <gustavo.guevara.collet@gmail.com>
2024-10-22 18:49:54 -0400

    task 4 betty docked

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/*
+ *
+ * Description: 'print without printf the text
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
