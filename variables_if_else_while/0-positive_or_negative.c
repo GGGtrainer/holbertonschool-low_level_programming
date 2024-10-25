f4284bdf403d60ca8e321ec32231320ee9e2b319
GGGtrainer <gustavo.guevara.collet@gmail.com>
2024-10-25 10:33:08 -0400

    program that specifies positive, negative or zero

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*file header means?? main?!*/
+
+/**
+ * main - printf
+ * @main - positive negative or zero
+ * Return: Always (0)
+**/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+	{
+		printf("%n is zero");
+	}
+	else if (n < 0)
+	{
+		printf("%n is negative");
+	}
+	else
+	{
+	printf("%n is positive");
+	}
+	return (0);
+}
