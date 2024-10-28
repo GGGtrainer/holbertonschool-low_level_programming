24833fecb0762f02953d657de53e4a5f1cc96c09
GGGtrainer <gustavo.guevara.collet@gmail.com>
2024-10-27 21:09:29 -0400

    Printing the alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+
 /**
  * main - entry point
- * @main - print alphabet
+ * @main - C program to print alphabet a-z lowercase
  * Return: Always (0)
 **/
+
 int main(void)
+{
+	char i;
+	
+	for(i='a'; i<='z'; i++)
+	{
+		putchar(i);
+	}
+	printf("\n");
+	return (0);
+}
