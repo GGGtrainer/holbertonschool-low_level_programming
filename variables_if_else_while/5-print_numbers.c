610f123fcb8cbe862e9fc6fad16c15aef5cdec54
GGGtrainer <gustavo.guevara.collet@gmail.com>
2024-10-27 22:56:32 -0400

    printing base numbers single digit

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Print every single digit base 10 number
+ * Result: Always 0
+**/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	printf("%d\n", i);
+	}
+	return (0);
+}
