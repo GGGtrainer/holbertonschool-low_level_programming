787863d9965fe2e78d0e148293cb9bb2491c4abd
GGGtrainer <gustavo.guevara.collet@gmail.com>
2024-10-30 13:32:17 -0400

    printing numbers a char

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @main - Print every single digit base 10 number
+ * Return: Always 0
+**/
+int main(void)
+{
+	int ch;
+	
+	for (ch = 48; ch <= 57; ch++)	
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}            
