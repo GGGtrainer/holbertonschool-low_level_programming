95df9d4b00502e4f38bc46e71bd001fb41b7f896
GGGtrainer <gustavo.guevara.collet@gmail.com>
2024-11-08 13:30:27 -0400

    prints alphabet using _putchar

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,17 @@
 #include "main.h"
-
 /**
- * main - check code
+ * print_alphabet - prints alphabet in lowercase
  *
- * Return: Always 0
-**/
-int main(void)
+ * Return: Always void (Success)
+ */
+
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
 }
