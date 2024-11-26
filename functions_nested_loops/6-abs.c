0808a977b003a6ca360d23ad6d7de5328ad87054
GGGtrainer <gustavo.guevara.collet@gmail.com>
2024-11-26 15:41:47 -0400

    absolute value

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -12,10 +12,10 @@ int _abs(int c)
 {
 	if (c < 0)
 	{
-		int abs_val;
+		int abs_value;
 
-		abs_val = c * -1;
-		return (abs_val);
+		abs_value = c * -1;
+		return (abs_value);
 	}
 	return (c);
 }
