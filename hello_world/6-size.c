10b22439b8336c60945dedbd834c71ee7bb33583
GGGtrainer <9716@holbertonstudents.com>
2025-02-26 20:48:08 -0400

    sizes of things

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	float f;
 
 	printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of an int: %d byte(s).\n", (unsigned long)sizeof(i));
+	printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(l));
 	printf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %d byte(s)\n", (unsigned long)sizeof(f));
