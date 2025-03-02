1b21e4ffe3d64aa4bd27d64d92f89e254fbc4f15
GGGtrainer <9716@holbertonstudents.com>
2025-03-02 17:40:57 -0400

    checker

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiply two ints
+ * @a: num to mul
+ * @b: num to mul
+ * Return: the product
+ */
+
+int mul(int a, int b)
+{
+	int prod = a * b;
+
+	return (prod);
+}
