60ac9424e3c568bf4a2875634fd952a7332eafea
GGGtrainer <9716@holbertonstudents.com>
2025-03-02 17:14:26 -0400

    check

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isupperer - check for uppercase chars
+ * _isupper - check for uppercase chars
  * @c: char to be checked
  *
  * Return: 1 if char is upper, 0 otherwise.
@@ -8,9 +8,13 @@
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c<= 'Z')
+
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
 	else
+	{
 		return (0);
+	}
+}
