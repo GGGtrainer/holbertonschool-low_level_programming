c01a5d0f5887dc0cc25fad2d38d7ca815a37e95b
GGGtrainer <9716@holbertonstudents.com>
2025-03-02 23:15:20 -0400

    CHeck this again

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,21 +1,39 @@
 #include "main.h"
 
 /**
- * string_toupper - changing string to upper function
- * @strs : string pointer
- * Return: strs.
+ * cap_string - capitalizing all first letters
+ * @str : string pointer
+ * Return: str
  */
 
-char *string_toupper(char *strs)
+char *cap_string(char *str)
 {
 	int a;
 
-	for (a = 0; strs[a] != '\0'; a++)
+	for (a = 0; str[a] != '\0'; a++)
 	{
-		if (strs[a] >= 'a' && strs[a] <= 'z')
+		if (str[a] == ' '
+		    || str[a] == '\t'
+		    || str[a] == '\n'
+		    || str[a] == '.'
+		    || str[a] == ','
+		    || str[a] == '!'
+		    || str[a] == '?'
+		    || str[a] == '"'
+		    || str[a] == '('
+		    || str[a] == ')'
+		    || str[a] == '{'
+		    || str[a] == '}')
 		{
-			strs[a] = strs[a] - 32;
+			a++;
+			if (str[a] >= 'A' && str[a] <= 'Z')
+				a++;
+			else if (str[a] >= 'a' && str[a] <= 'z')
+				str[a] = str[a] - 32;
+			a--;
 		}
+		else if (str[0] >= 'a' && str[0] <= 'z')
+			str[0] = str[0] - 32;
 	}
-	return (strs);
+	return (str);
 }
