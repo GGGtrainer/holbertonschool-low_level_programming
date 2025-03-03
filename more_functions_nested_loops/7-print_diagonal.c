e24310b418f734bdd7bf361386d30ab60d16139e
GGGtrainer <9716@holbertonstudents.com>
2025-03-02 22:35:33 -0400

    diag

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - Draws a line in the terminal.
+ *
+ * @n: int used to check
+ */
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+				_putchar(' ');
+			_putchar('\\');
+
+			if (b == n - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
