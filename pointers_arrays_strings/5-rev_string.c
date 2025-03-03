4a1865bc86b8f1e570b236bdd01379bfb19f2974
GGGtrainer <9716@holbertonstudents.com>
2025-03-02 22:58:09 -0400

    Ill be back

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse
+ * rev_string - prints a string in reverse
  * @s: pointer to the string
  * return: void
  */
 
-void print_rev(char *s)
+void rev_string(char *s)
 {
 	int b = 0;
 
