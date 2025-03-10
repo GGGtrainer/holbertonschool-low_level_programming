8a4d91b98f445574c86377529039fbe5f5b9947e
GGGtrainer <9716@holbertonstudents.com>
2025-03-09 22:50:59 -0400

    dont forget hoe to comment correctly

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,13 +1,12 @@
 #include "main.h"
-/*
- * _putchar_recursion - Prints a string with a new line
+/**
+ * _puts_recursion - Prints a string with a new line.
  * @s: char
- * return: void
  */
 
 void _puts_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
